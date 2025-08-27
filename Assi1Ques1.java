e63393594d209bf63bdd886bcb4f56d43677ec8c
NitinKumar1054 <nitin2911.in@gmail.com>
2025-08-27 09:57:36 +0530

    Assignment1 java Question 1

---
+++ Assi1Ques1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Assi1Ques1 {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      float s = (float)n/2*(1 +n);
+      System.out.println((int)s);
+
+    }
+    
+}
